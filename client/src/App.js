09f3e18bd1684b89d60d8fc02fd30fd33fd7d345
Muhammed Shittu <devshittu@gmail.com>
2023-04-05 14:58:08 +0100

    used io socket in the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
+import React from "react"
 import './App.css';
 import {io} from "socket.io-client"
+
+const socket = io.connect("http://localhost:4000")
+
 function App() {
   return (
     <div className="App">
