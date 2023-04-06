5586dd66bab9c7b0be0ecb6737da586ee3adb367
Muhammed Shittu <devshittu@gmail.com>
2023-04-06 16:44:19 +0100

    add Socket.io to the project to create a real-time connection.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+
+const http = require("http").Server(app);
+const cors = require("cors");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+//👇🏻 Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on('disconnect', () => {
+      socket.disconnect()
+      console.log('🔥: A user disconnected');
+    });
+});
+
 
 app.get("/api", (req, res) => {
     res.json({
