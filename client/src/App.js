e27c80773ddb4ed4cc69cda2fe32c1a402367b91
Muhammed Shittu <devshittu@gmail.com>
2023-04-06 16:53:41 +0100

    Rendered the newly created components on different routes via React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,39 @@
 import React from "react"
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Photos from "./components/Photos";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import UploadPhoto from "./components/UploadPhoto";
+import MyPhotos from "./components/MyPhotos";
+import SharePhoto from "./components/SharePhoto";
+
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import './App.css';
 import {io} from "socket.io-client"
 
-const socket = io.connect("http://localhost:4000")
-
 function App() {
+  const socket = io.connect("http://localhost:4000")
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login socket={socket} />} />
+                    <Route path='/register' element={<Register socket={socket} />} />
+                    <Route path='/photos' element={<Photos socket={socket} />} />
+                    <Route
+                        path='/photo/upload'
+                        element={<UploadPhoto socket={socket} />}
+                    />
+                    <Route path='/user/photos' element={<MyPhotos socket={socket} />} />
+                    <Route path='/share/:user' element={<SharePhoto socket={socket} />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer />
+        </>
   );
 }
 
