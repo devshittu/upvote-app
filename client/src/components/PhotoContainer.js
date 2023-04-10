fed537e3e4af269791cdd59614740f872c448a89
Muhammed Shittu <devshittu@gmail.com>
2023-04-10 17:57:32 +0100

    Update the handleUpvote function within the PhotoContainer.js to send the user and the image ID to the server.

--- client/src/components/PhotoContainer.js
+++ client/src/components/PhotoContainer.js
@@ -1,35 +1,38 @@
-import React, { useEffect } from "react";
-import { MdOutlineArrowUpward } from "react-icons/md";
+import React, { useEffect } from 'react';
+import { MdOutlineArrowUpward } from 'react-icons/md';
 
 const PhotoContainer = ({ photos, socket }) => {
-    const handleUpvote = (id) => {
-        console.log("Upvote", id);
-    };
+  const handleUpvote = (id) => {
+    socket.emit('photoUpvote', {
+      userID: localStorage.getItem('_id'),
+      photoID: id,
+    });
+  };
 
-    return (
-        <main className='photoContainer'>
-            {photos.map((photo) => (
-                <div className='photo' key={photo.id}>
-                    <div className='imageContainer'>
-                        <img
-                            src={photo.image_url}
-                            alt={photo.id}
-                            className='photo__image'
-                        />
-                    </div>
+  return (
+    <main className="photoContainer">
+      {photos.map((photo) => (
+        <div className="photo" key={photo.id}>
+          <div className="imageContainer">
+            <img
+              src={photo.image_url}
+              alt={photo.id}
+              className="photo__image"
+            />
+          </div>
 
-                    <button className='upvoteIcon' onClick={() => handleUpvote(photo.id)}>
-                        <MdOutlineArrowUpward
-                            style={{ fontSize: "20px", marginBottom: "5px" }}
-                        />
-                        <p style={{ fontSize: "12px", color: "#ce7777" }}>
-                            {photo.vote_count}
-                        </p>
-                    </button>
-                </div>
-            ))}
-        </main>
-    );
+          <button className="upvoteIcon" onClick={() => handleUpvote(photo.id)}>
+            <MdOutlineArrowUpward
+              style={{ fontSize: '20px', marginBottom: '5px' }}
+            />
+            <p style={{ fontSize: '12px', color: '#ce7777' }}>
+              {photo.vote_count}
+            </p>
+          </button>
+        </div>
+      ))}
+    </main>
+  );
 };
 
 export default PhotoContainer;
